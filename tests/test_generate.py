62ba9ed0d382c9edb454614020ea132532f0e692
slapaay <slapelachie@gmail.com>
2020-04-18 12:17:33 +1000

    Updated tests

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -15,37 +15,37 @@ class TestUtils(unittest.TestCase):
 	def test_generate_one(self):
 		# Testing for generating one theme
 		shutil.rmtree(out_dir, ignore_errors='FileNotFoundError')
-		theme.generate('tests/assets/test.jpg', template_dir, out_dir)
+		theme.generate('tests/assets/test.jpg', out_dir)
 
 		self.assertIs(os.path.isfile(os.path.join(out_dir,
-			'themes/31084f2c8577234aeb55-colors.sh')), True)
+			'themes/31084f2c8577234aeb55.json')), True)
 		self.assertIs(os.path.isfile(os.path.join(out_dir,
-			'themes/31084f2c8577234aeb55-Xdefaults')), True)
+			'themes/31084f2c8577234aeb55.json')), True)
 
 	def test_generate_all(self):
 		# Testing for generating all images into themes
 		shutil.rmtree(out_dir, ignore_errors='FileNotFoundError')
-		theme.generate('tests/assets/', template_dir, out_dir)
+		theme.generate('tests/assets/', out_dir)
 
-		self.assertIs(os.path.isfile(os.path.join(out_dir,
-			'themes/31084f2c8577234aeb55-colors.sh')), True)
-		self.assertIs(os.path.isfile(os.path.join(out_dir,
-			'themes/31084f2c8577234aeb55-Xdefaults')), True)
+		self.assertTrue(os.path.isfile(os.path.join(out_dir,
+			'themes/31084f2c8577234aeb55.json')))
+		self.assertTrue(os.path.isfile(os.path.join(out_dir,
+			'themes/31084f2c8577234aeb55.json')))
 
 	def test_update(self):
 		# Test if update passes when theme file exists
-		theme.update('tests/assets/test.jpg', out_dir)
-
-		self.assertEqual(os.readlink(os.path.join(out_dir, 'colors.sh')),
-			os.path.join(out_dir, 'themes/31084f2c8577234aeb55-colors.sh'))
-		self.assertEqual(os.readlink(os.path.join(out_dir, 'Xdefaults')),
-			os.path.join(out_dir, 'themes/31084f2c8577234aeb55-Xdefaults'))
+		theme.update('tests/assets/test.jpg', out_dir, template_dir)
+		
+		self.assertTrue(os.path.isfile(os.path.join(out_dir,
+			'colors.sh')))
+		self.assertTrue(os.path.isfile(os.path.join(out_dir,
+			'Xdefaults')))
 	
 	def test_update_fail(self):
 		# Test if fail if theme file does not exist
 		shutil.rmtree(out_dir, ignore_errors='FileNotFoundError')
 		try:
-			theme.update('tests/assets/test.jpg', out_dir)
+			theme.update('tests/assets/test.jpg', out_dir, template_dir)
 			raise ValueError("Passed Succesfully?!")
 		except theme.noPreGenThemeError:
 			pass
