b3ee1904731a86e7d43607092610fe7c7af38b2e
Lachlan Slape <33539682+slapelachie@users.noreply.github.com>
2020-07-23 16:49:35 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 LONG_DESC = open('README.md').read()
 
 setup(name='KADAI',
-	version='1.4',
+	version='1.3',
 	description='Simple wallpaper manager for tiling window managers.',
 	long_description_content_type="text/markdown",
 	long_description=LONG_DESC,
